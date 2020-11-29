ea48b0c5eda36dc19d387896742be13a05ab0bc4
marco741 <marco741147@gmail.com>
2020-11-29 18:19:48 +0100

    Added list users tests

--- test/test_user.py
+++ test/test_user.py
@@ -14,14 +14,36 @@ def test_get_user_not_found(client, unexisting_user, reset_db):
     assert 'message' in res.get_json().keys()
 
 
+def test_get_users_success(client, user_seeds, reset_db):
+    test_current_page = 1
+    test_page_size = len(user_seeds) - 1
+
+    res = client.get(
+        f"/users?current_page={test_current_page}&page_size={test_page_size}")
+
     assert res.status_code == 200
-    data = res.get_json()
-    assert len(data['rows']) == 3
+    assert len(res.get_json()['rows']) <= test_page_size
+    assert res.get_json()['meta']['count'] == len(user_seeds)
+    assert res.get_json()['meta']['current_page'] == test_current_page
+    assert res.get_json()['meta']['page_size'] == test_page_size
+
+    import math
+    expected_page_count = math.ceil(len(user_seeds) / test_page_size)
+    assert res.get_json()['meta']['page_count'] == expected_page_count
 
 
-def test_post_user_success(client, seed):
-    res = client.post(
-        '/user', data=test_user)
+def test_get_users_page_not_found(client, user_seeds, reset_db):
+    test_page_size = 5
+    import math
+    test_page_count = math.ceil(len(user_seeds) / test_page_size)
+    test_current_page = test_page_count + 1
+    res = client.get(
+        f"/users?current_page={test_current_page}&page_size={test_page_size}")
+
+    assert res.status_code == 404
+    assert "message" in res.get_json().keys()
+
+
     assert res.status_code == 201
     assert isinstance(res.get_json(), dict)
     assert res.get_json()['username'] == test_user['username']
