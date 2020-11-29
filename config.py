836a0fce48729673f442185f8edac7f5a4d10fae
marco741 <marco741147@gmail.com>
2020-11-29 18:45:20 +0100

    removes SECRET_KEY and get_env_variable function

--- config.py
+++ config.py
@@ -1,22 +1,12 @@
 from os import getenv
-from common.utils import get_env_variable
-
 from dotenv import load_dotenv
-from pathlib import Path  # python3 only
+from pathlib import Path
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
+# Documenta tutte le flag
 
-# Dynamic (Environmental) configurations
-
-# Crashes if not present in environment
-SECRET_KEY = get_env_variable('SECRET_KEY')
-
-# TODO: Change default database url
-# DB_URL is postgres one if launched from Heroku or sqlite one for local testing
 DB_URL = getenv("DATABASE_URL", 'sqlite:///data.db')
 TEST_DB_URL = getenv("TEST_DATABASE_URL", 'sqlite:///data.db')
-
-# Debug and testing only if explicitly stated in environment
 DEBUG = getenv("DEBUG") == "TRUE"
 TESTING = getenv("TESTING") == "TRUE"
 ENVIRONMENT = "development" if getenv(
@@ -37,7 +27,7 @@ class Config:
 
 
 class TestConfig:
-    """Flask config class."""
+    """Flask test config class."""
     SQLALCHEMY_DATABASE_URI = TEST_DB_URL
 
     # silence the deprecation warning
