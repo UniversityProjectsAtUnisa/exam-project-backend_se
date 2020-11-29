709d4ac7855d7b5bb366f6e7e789360b0eaa3f9b
marco741 <marco741147@gmail.com>
2020-11-29 18:19:28 +0100

    Added get user tests

--- test/test_user.py
+++ test/test_user.py
@@ -1,8 +1,19 @@
-test_user = {'username': 'marco741', 'password': 'password', 'role': 'admin'}
+def test_get_user_success(client, user_seeds, reset_db):
+    test_user = user_seeds[0]
+    res = client.get(f"/user/{test_user['username']}")
+    assert res.status_code == 200
+    assert res.get_json()['username'] == test_user['username']
+    assert res.get_json()['role'] == test_user['role']
+    assert 'password' not in res.get_json().keys()
 
 
-def test_get_users(client, seed):
-    res = client.get('/users')
+def test_get_user_not_found(client, unexisting_user, reset_db):
+    test_user = unexisting_user
+    res = client.get(f"/user/{test_user['username']}")
+    assert res.status_code == 404
+    assert 'message' in res.get_json().keys()
+
+
     assert res.status_code == 200
     data = res.get_json()
     assert len(data['rows']) == 3
