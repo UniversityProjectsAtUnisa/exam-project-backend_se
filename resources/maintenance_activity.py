caefaeff40b11029041cfff0770cc2751750501c
marco741 <marco741147@gmail.com>
2020-12-14 01:39:46 +0100

    Added role_required decorator on MaintenanceActivity resource methods

--- resources/maintenance_activity.py
+++ resources/maintenance_activity.py
@@ -1,15 +1,11 @@
 from models.maintenance_activity import MaintenanceActivityModel
 from flask_restful import Resource, reqparse
+from jwt_utils import role_required
 
 
 class MaintenanceActivity(Resource):
     """User API for get (single), put and delete operations."""
     _activity_parser = reqparse.RequestParser()
-    _activity_parser.add_argument("activity_id",
-                                  type=int,
-                                  required=False,
-                                  help="ID should be unique"
-                                  )
     _activity_parser.add_argument("workspace_notes",
                                   type=str,
                                   required=False,
@@ -17,6 +13,7 @@ class MaintenanceActivity(Resource):
                                   )
 
     @classmethod
+    @role_required("planner")
     def get(cls, id):
         """Gets one activity from database based on given id. 
             Fails if there is no activity with that id.
@@ -37,6 +34,7 @@ class MaintenanceActivity(Resource):
         return activity.json(), 200
 
     @classmethod
+    @role_required("planner")
     def put(cls, id):
         """Edits one activity in the database based on given id. 
             Fails if there is no activity with that id.
@@ -61,6 +59,7 @@ class MaintenanceActivity(Resource):
         return activity.json(), 200
 
     @classmethod
+    @role_required("planner")
     def delete(cls, id):
         """Deletes one activity from database based on given id. 
             Fails if there is no activity with that id.
@@ -92,10 +91,11 @@ class MaintenanceActivityList(Resource):
                                   )
     _activity_parser.add_argument("page_size",
                                   type=int,
-                                  default=3
+                                  default=10
                                   )
 
     @classmethod
+    @role_required("planner")
     def get(cls):
         """Gets a paginated list of activites, along with its metadata. Takes current_page and page_size as optional body arguments.
 
@@ -114,12 +114,6 @@ class MaintenanceActivityList(Resource):
 class MaintenanceActivityCreate(Resource):
     """Maintenance Activity API for post operations."""
     _activity_parser = reqparse.RequestParser()
-    _activity_parser = reqparse.RequestParser()
-    _activity_parser.add_argument("activity_id",
-                                  type=int,
-                                  required=True,
-                                  help="ID should be unique"
-                                  )
     _activity_parser.add_argument("activity_type",
                                   type=str,
                                   required=True,
@@ -166,10 +160,10 @@ class MaintenanceActivityCreate(Resource):
                                   help="Workspace Notes should be a short description of the workspace"
                                   )
 
-    @ classmethod
+    @classmethod
+    @role_required("planner")
     def post(cls):
         """Creates one activity in the database. 
-            Fails if there is already an activity with that id.
 
         Returns:
             dict of (str, any): Jsonified activity or error message.
@@ -177,9 +171,6 @@ class MaintenanceActivityCreate(Resource):
         data = cls._activity_parser.parse_args()
 
         try:
-            if MaintenanceActivityModel.find_by_id(data["activity_id"]):
-                return {"message": "Maintenance Activity with id '{}' already exists".format(data["activity_id"])}, 400
-
             activity = MaintenanceActivityModel(**data)
             activity.save_to_db()
         except Exception as e:
